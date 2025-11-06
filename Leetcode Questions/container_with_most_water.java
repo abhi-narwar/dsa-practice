8c48debd400d659e0210bf8b2b20474ee821372e
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-06 16:21:50 +0530

    added code

---
+++ Leetcode Questions/container_with_most_water.java
@@ -0,0 +1,18 @@
+class container_with_most_water{
+    public int maxArea(int[] arr) {
+        int max=0;
+        int i=0;
+        int j=arr.length-1;
+        while(i<=j){
+            int area=Math.min(arr[i],arr[j])*(j-i);
+            max=Math.max(area,max);
+            if(arr[i]<arr[j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return max;
+        
+    }
+}
