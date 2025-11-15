eca2664f9decc733ee4f39161c33a03617f0c3a2
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-15 15:24:18 +0530

    added code

--- Leetcode Questions/remove_duplicates_from_sorted_array.java
+++ Leetcode Questions/remove_duplicates_from_sorted_array.java
@@ -1,9 +1,9 @@
-class remove_duplicates_from_sorted_array_ll {
+class remove_duplicates_from_sorted_array {
     public int removeDuplicates(int[] nums) {
-        int n=nums.length;
-        int i=2;
-        for(int j=2;j<n;j++){
-            if(nums[j]!=nums[i-2]){
+        int n = nums.length;
+        int i=1;
+        for(int j=1;j<n;j++){
+            if(nums[j]!=nums[i-1]){
                 nums[i]=nums[j];
                 i++;
             }
--- Leetcode Questions/remove_duplicates_from_sorted_array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
-        i=0
-        for j in range(1,len(nums)):
-            if nums[j]!=nums[j-1]:
-                nums[i+1]=nums[j]
-                i+=1
-        return i+1
-        
