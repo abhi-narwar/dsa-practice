049ac406dec1e126660a0065d559ddbbbc704e9c
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-19 20:06:22 +0530

    added code

---
+++ Interview-daily-practice/jump_game.java
@@ -0,0 +1,15 @@
+//greedy approach
+class jump_game {
+    public boolean canJump(int[] nums) {
+        int reach=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>reach){
+                return false;
+            }
+            reach=Math.max(reach,i+nums[i]);
+        }
+        return true;
+
+
+    }
+}
