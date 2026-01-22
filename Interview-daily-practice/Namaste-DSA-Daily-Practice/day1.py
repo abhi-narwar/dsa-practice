9de80787b746fc8b4dca2f4826f416a1a6fedf3c
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2026-01-22 09:36:55 +0530

    added code

--- Interview-daily-practice/Namaste-DSA-Daily-Practice/day1.py
+++ Interview-daily-practice/Namaste-DSA-Daily-Practice/day1.py
@@ -5,7 +5,7 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-
+                
 
 # This is an optimized solution with O(n) time complexity using a hash map.
 
