96ac4b3d0eb053b3898d59e318b7b46b45706281
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2026-01-22 09:25:52 +0530

    added code

--- Interview-daily-practice/Namaste-DSA-Daily-Practice/day2.py
+++ Interview-daily-practice/Namaste-DSA-Daily-Practice/day2.py
@@ -11,3 +11,24 @@ class Solution:
         
         digits = sorted(digits)
         return digits[-2]
+
+
+
+#O(n) time complexity solution O(1) space complexity
+
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        largest = -1
+        second_largest = -1
+
+        for ch in s:
+            if ch.isdigit():
+                digit = int(ch)
+
+                if digit > largest:
+                    second_largest = largest
+                    largest = digit
+                elif largest > digit > second_largest:
+                    second_largest = digit
+
+        return second_largest
