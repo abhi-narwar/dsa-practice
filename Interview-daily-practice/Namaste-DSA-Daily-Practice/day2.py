34352392c47db9b6c9d5ae4bddbc5adc1f0f33ad
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2026-01-22 09:07:45 +0530

    added code

---
+++ Interview-daily-practice/Namaste-DSA-Daily-Practice/day2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        digits = set()
+        
+        for i in s:
+            if i.isdigit():
+                digits.add(int(i))
+        
+        if len(digits) < 2:
+            return -1
+        
+        digits = sorted(digits)
+        return digits[-2]
