ec5354a2677cc8958c81dc07c22c1046ffd7a7f4
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2026-01-25 18:20:18 +0530

    added code

---
+++ Interview-daily-practice/Namaste-DSA-Daily-Practice/day14.py
@@ -0,0 +1,29 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+
+#O(n)  ---time complexity
+#O(1)  ---space complexity
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[j]=nums[i]
+                j+=1
+        for k in range(j, len(nums)):
+            nums[k] = 0
+    
+
+#O(n)  ---time complexity
+#O(1)  ---space complexity
